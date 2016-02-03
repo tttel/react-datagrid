f50ad0a1c22dd4ae79042f0cc71b24cf1f8392fd
Bogdan Petru Pintican <bogdanpintican@gmail.com>
2016-02-03 13:36:59 +0200

    Add dummy column component

---
+++ src/Column/index.js
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react'
+import Component from 'react-class'
+
+
+/**
+ *  This is a dummy component
+ *  it's purpose it for configuration of columns
+ */
+export default class Column {
+  render(){
+    return <div {...this.props} />
+  }
+} 
