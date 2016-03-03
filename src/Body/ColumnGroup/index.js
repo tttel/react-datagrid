451a7f2da9568888ebcc4d5b8efff929a9562ec9
bogdanpetru <bogdanpintican@gmail.com>
2016-03-03 21:42:13 +0200

    Remove unused index prop, remove waisted time

--- src/Body/ColumnGroup/index.js
+++ src/Body/ColumnGroup/index.js
@@ -84,7 +84,7 @@ export default class ColumnGroup extends Component {
       )
 
     const innerWrapperStyle = {
-      transform: `translateY(${innerWrapperOffset}px)` 
+      // transform: `translateY(${innerWrapperOffset}px)` 
     }
 
     return <div 
@@ -154,7 +154,7 @@ export default class ColumnGroup extends Component {
         even,
         over,
         active,
-        index,
+        // index,
         key,
         renderRow,
         cellFactory,
