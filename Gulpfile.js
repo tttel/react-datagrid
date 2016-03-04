2915cccb76e42fb5b17b079ddbeb09804f02346e
Bogdan Petru Pintican <bogdanpintican@gmail.com>
2016-03-04 10:45:23 +0200

    Update buildStatic

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,47 @@
-var gulp  = require('gulp');
-var babel = require('gulp-babel');
+var gulp  = require('gulp')
+var babel = require('gulp-babel')
+var file  = require('gulp-file')
 
-gulp.task('default', function () {
-    return gulp.src('./src/**')
-        .pipe(babel())
-        .pipe(gulp.dest('./lib'));
-});
+var packageJSON = require('./package')
+
+function getQuery(){
+  var date    = new Date()
+  var version = packageJSON.version
+  var query   = version + '__'
+
+  return query + [
+    date.getFullYear(),
+    date.getMonth() + 1,
+    date.getDate()
+  ].join('-')
+  + '_' +
+  [
+    date.getHours(),
+    date.getMinutes(),
+    date.getMilliseconds(),
+  ].join(':')
+}
+
+
+gulp.task('build-html', function(){
+  require('babel-core/register')
+
+  var IndexPage = require('./build/IndexPage')
+  var query2 = getQuery();
+
+  var indexStr = IndexPage.renderToStaticMarkup({
+    query: query2
+  })
+
+  file('index.html', indexStr, {src: true})
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('copy-react', function(){
+  gulp.src('./node_modules/react/dist/react.min.js')
+     .pipe(gulp.dest('./dist'))
+  gulp.src('./node_modules/react-dom/dist/react-dom.min.js')
+     .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('default', ['build-html', 'copy-react'])
