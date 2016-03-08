f8ad197f85c84f955b12a658e703b3f92dbd9224
Bogdan Petru Pintican <bogdanpintican@gmail.com>
2016-03-08 11:41:05 +0200

    Fix zebRows to not change on each render

--- src/Body/getDataRangeToRender.js
+++ src/Body/getDataRangeToRender.js
@@ -1,6 +1,16 @@
 const getDataRangeToRender = (bodyHeight, itemHeight, scrollTop, extraRows = 4) => {
-  const noItemsToRender = parseInt(bodyHeight / itemHeight + extraRows, 10)
-  
+  let noItemsToRender = parseInt(bodyHeight / itemHeight + extraRows, 10)
+
+  // ajust from and to so that rows have the same
+  // odd and even classNames
+  // from must be even
+  // and to must be od
+
+  // noItemsToRender must be odd
+  if (noItemsToRender % 2 === 0) {
+    noItemsToRender += 1
+  }
+
   if (scrollTop <= (extraRows/2) * itemHeight) {
     // reder first n items
     return {
@@ -9,7 +19,13 @@ const getDataRangeToRender = (bodyHeight, itemHeight, scrollTop, extraRows = 4) 
     }
   }
 
-  const renderFrom = parseInt(scrollTop / itemHeight - (extraRows / 2), 10)
+  let renderFrom = parseInt(scrollTop / itemHeight - (extraRows / 2), 10)
+  
+  // fisrt must be even
+  if (renderFrom % 2 !== 0) {
+    renderFrom += 1  
+  }
+
   const rangeToRender = {
     from: renderFrom,
     to: renderFrom + noItemsToRender,
